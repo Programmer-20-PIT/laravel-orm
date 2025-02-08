1ca2984da51f14be69aa1847ee760954734edbad
Arizab02 <arizab002@gmail.com>
2025-02-08 16:03:21 +0700

    Langkah terakhir mas El

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('pages.dashboard');
-});
 
-Route::get('/login', function () {
-    return view('pages.login');
-});
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth'])->group(function () {
 
-    Route::get('/contacts', function () {
-        return view('contact');
+    Route::get('/home', action: function () {
+        return view('pages.dashboard');
     });
 });
