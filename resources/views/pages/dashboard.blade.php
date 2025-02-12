4b3b1486ae96a7707b132aa0e1d65f5f9db5a08f
ELGhifari11 <ghifariakun@gmail.com>
2025-02-12 13:32:24 +0700

    Mode Edit di list table

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -126,29 +126,10 @@
                                     </tbody>
                                     @endforeach
                                 </table>
-
-                                <nav aria-label="...">
-                                    <ul class="pagination">
-                                        <li class="page-item disabled">
-                                            <a class="page-link"
-                                                href="#"
-                                                tabindex="-1">Previous</a>
-                                        </li>
-                                        <li class="page-item"><a class="page-link"
-                                                href="#">1</a></li>
-                                        <li class="page-item active">
-                                            <a class="page-link"
-                                                href="#">2 <span class="sr-only">(current)</span></a>
-                                        </li>
-                                        <li class="page-item"><a class="page-link"
-                                                href="#">3</a></li>
-                                        <li class="page-item">
-                                            <a class="page-link"
-                                                href="#">Next</a>
-                                        </li>
-                                    </ul>
-                                </nav>
-
+                                <div
+                                class="float-right mt-3 mb-3 card-footer page-item disa ">
+                                    {{ $data->withQueryString()->links() }}
+                                </div>
                             </div>
                     </div>
 
